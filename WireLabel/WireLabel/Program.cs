b727f66e00f8c5409a17344330fb6c89b1a6204a
MrSbkz <mr.sobakaz@gmail.com>
2022-03-24 19:15:09 +0300

    1 - Create app skeleton

---
+++ WireLabel/WireLabel/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WireLabel.Services;
+
+namespace WireLabel;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Host.CreateDefaultBuilder()
+            .ConfigureServices(ConfigureServices)
+            .ConfigureHostConfiguration(config =>
+            {
+                config.AddJsonFile("appsettings.json")
+                    .AddEnvironmentVariables()
+                    .Build();
+            })
+            .ConfigureServices(services => services.AddSingleton<WorkerService>())
+            .Build()
+            .Services
+            .GetService<WorkerService>()
+            ?.Start();
+    }
+
+    private static void ConfigureServices(HostBuilderContext hostContext, IServiceCollection services)
+    {
+    }
+}
---
+++ WireLabel/WireLabel/Services/WorkerService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace WireLabel.Services;
+
+public class WorkerService
+{
+    private readonly IConfiguration _configuration;
+
+    public WorkerService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void Start()
+    {
+        var path = _configuration["SavePath"];
+
+        Console.WriteLine(path);
+    }
+}
